31079ccf989108fbd8507cebb1647d932bf5b6b9
HNK-hub <haticenur.4455@gmail.com>
2024-04-01 11:52:00 +0300

    #5 route

--- routes/web/api.php
+++ routes/web/api.php
@@ -5,26 +5,12 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('v1')->group(function () {
-    // Kategori rotaları
-    Route::get('categories', [CategoryController::class, 'index']);
-    Route::get('categories/{id}', [CategoryController::class, 'show']);
-    Route::post('categories', [CategoryController::class, 'store']);
-    Route::put('categories/{id}', [CategoryController::class, 'update']);
-    Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
-
-    // Yazar rotaları
-    Route::get('authors', [AuthorController::class, 'index']);
-    Route::get('authors/{id}', [AuthorController::class, 'show']);
-    Route::post('authors', [AuthorController::class, 'store']);
-    Route::put('authors/{id}', [AuthorController::class, 'update']);
-    Route::delete('authors/{id}', [AuthorController::class, 'destroy']);
-
-    // Kitap rotaları
-    Route::get('books', [BookController::class, 'books.index']);
-    Route::get('books/{id}', [BookController::class, 'show']);
-    Route::post('books', [BookController::class, 'store']);
-    Route::put('books/{id}', [BookController::class, 'update']);
-    Route::delete('books/{id}', [BookController::class, 'destroy']);
+Route::prefix('api/v1')->group(function () {
+    Route::prefix('books')->group(function () {
+        Route::get('/', 'BookController@index');
+        Route::post('/', 'BookController@store');
+        Route::get('/{book}', 'BookController@show');
+        Route::put('/{book}', 'BookController@update');
+        Route::delete('/{book}', 'BookController@destroy');
+    });
 });
