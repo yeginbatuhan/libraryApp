7896d65dddeda5235406352c5c02f4b5fa5c5fd8
HNK-hub <haticenur.4455@gmail.com>
2024-04-18 14:52:54 +0300

    #17 Last Update

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,14 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LaravelApp</title>
+    <title>TEGV</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light text-center"
      style="background-color: rgba(0, 51, 87, 0.85); border-radius: 0 0 15% 15%;">
-    <a class="navbar-brand" href="#" style="color: white;">LaravelApp</a>
+    <a class="navbar-brand" href="#" style="color: white;">
+        <img src="{{ asset('storage/logo2.jpg') }}" height="50" alt="Logo" style="border-radius: 25px;">
+    </a>
     <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
