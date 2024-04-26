821db5b6cf5e65d2755077d132abc246db019476
HNK-hub <haticenur.4455@gmail.com>
2024-04-26 16:44:53 +0300

    #23 Last Update

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kitap Listesi</title>
     <!-- Bootstrap CSS ekleme -->
+    <!-- Bootstrap CSS ekleme -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -51,12 +52,17 @@
         @endforeach
         </tbody>
     </table>
+    <div class="d-flex justify-content-end">
+        {{ $books->links('pagination::bootstrap-4') }}
+    </div>
 </div>
 
 <!-- Bootstrap JS ve diğer bağımlılıklar -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Bootstrap CSS ekleme -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
 @include('partials.footer')
 </body>
